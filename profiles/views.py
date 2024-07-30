a4b3a8b550942e31a844f3f88b3ac4863048a83d
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-07-30 10:20:48 +0000

    Create profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 def profile(request):
     '''Display the user's profile'''
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
     
     return render(request, template, context)

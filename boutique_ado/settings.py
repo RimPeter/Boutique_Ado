3cc694bc0c2079107027ae17a3a32d027692a69e
peter rimaszecsi <hull@localappliancerentals.co.uk>
2024-08-05 09:19:46 +0000

    DEBUG = 'DEVELOPMENT' in os.environ

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False #'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-rimpeter-boutiqueado-qevh76jijpw.ws-eu115.gitpod.io', 'localhost', 'boutique-ado-peter-29b70c456421.herokuapp.com']
 
